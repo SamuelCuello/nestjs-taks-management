be83a7a5b4faec9ef683345e847844bc7ab67afe
SamuelCuello <samueleloycuello@gmail.com>
2022-04-16 13:26:32 -0400

    transformer interceptor created

--- src/tasks/Dto/task.entity.ts
+++ src/tasks/Dto/task.entity.ts
@@ -1,3 +1,4 @@
+import { Exclude } from "class-transformer";
 import { User } from "src/users/model.entity";
 import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
 import { TaskStatus } from '../tasks-status.enum';
@@ -18,5 +19,6 @@ description: string;
 status: TaskStatus;
 
 @ManyToOne((_type)=> User, (user)=> user.tasks, {eager: false})
+@Exclude({toPlainOnly: true})
 user: User
 }
