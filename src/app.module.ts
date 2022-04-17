c747dfefaf08efca373e4f4d85ae74c9a940a1a1
SamuelCuello <samueleloycuello@gmail.com>
2022-04-16 21:37:42 -0400

    Config To TypeOrm, sensitive information saved in env variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TasksModule } from './tasks/tasks.module';
 import { UsersModule } from './users/users.module';
@@ -8,21 +8,26 @@ import { UsersModule } from './users/users.module';
 @Module({
   imports: [
     ConfigModule.forRoot({
-      envFilePath: [`.env.stage, ${process.env.STAGE}`],
+      envFilePath: [`.env.stage.${process.env.STAGE}`],
     }),
     TasksModule, 
     UsersModule,
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host:'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'postgres',
-      database:'task-management',
-      autoLoadEntities: true,
-      synchronize: true,
-    })
+    TypeOrmModule.forRootAsync({
+      imports:[ConfigModule],
+      inject:[ConfigService],
+      useFactory: async (configService: ConfigService) =>({
+        type: 'postgres',
+        autoLoadEntities: true,
+        synchronize: true,
+        host: configService.get('DB_HOST'),
+        port: configService.get('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_DATABASE')
+      }),
 
+    }),
+    
   ],
 })
 export class AppModule {}
